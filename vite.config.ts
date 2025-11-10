cb10fdef8a3e943e6382eb9acd195a63559c1650
RichikaR <richikarani20@gmail.com>
2025-11-10 14:48:46 +0530

    Update .env for production backend

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/file-integrity-verification-system/", // Replace with your repo name
+  base: "/file-integrity-verification-system/", 
   plugins: [react()],
 });
