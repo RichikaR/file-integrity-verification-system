8d8ba52bd937dc5fc6a673f1d329f361ff425930
RichikaR <richikarani20@gmail.com>
2025-11-10 13:04:56 +0530

    Fix package.json and deploy scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/file-integrity-verification-system/", // Replace with your repo name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
